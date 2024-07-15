607a141bb35d134edcc36bc192f3478d349c4007
Scholiboi <115933593+Scholiboi@users.noreply.github.com>
2024-07-15 11:37:00 +0530

    first_commit

---
+++ 1st proj/script.js
@@ -0,0 +1,22 @@
+//document.getElementById("count-el").innerText = 5;
+let countel = document.getElementById("count-el");
+let count = 0;
+let saveel = document.getElementById("save-el");
+
+function increment() {
+    count = count + 1;
+    countel.innerText = count;
+}
+
+function save() {
+    let save = count;
+    saveel.innerText += save + "-";
+    countel.innerText = 0;
+    count = 0;
+}
+
+function clear() {
+    saveel.innerText = "Previous entries:";
+    countel.innerText = 0;
+    count = 0;
+}
